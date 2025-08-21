5fa762737005f3ae38fbf5ace7d3edbde512b987
MaitreJulian <julian.venka@outlook.be>
2025-08-21 14:19:44 +0200

    for -> while

--- src/copy_env.c
+++ src/copy_env.c
@@ -1,31 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   copy_env.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvenkata <jvenkata@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/21 14:13:40 by jvenkata          #+#    #+#             */
+/*   Updated: 2025/08/21 14:19:13 by jvenkata         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include <string.h>
 
-char **copy_env(char **envp)
+char	**copy_env(char **envp)
 {
-    int i, count;
-    char **env_copy;
+	int		i;
+	int		count;
+	char	**env_copy;
 
-    // Compter le nombre de variables d'environnement
-    for (count = 0; envp[count] != NULL; count++);
-
-    // Allouer de la mémoire pour le nouveau tableau (compter +1 pour NULL final)
-    env_copy = malloc((count + 1) * sizeof(char *));
-    if (!env_copy)
-        return NULL;
-
-    // Dupliquer chaque variable
-    for (i = 0; i < count; i++) {
-        env_copy[i] = strdup(envp[i]);
-        if (!env_copy[i]) {
-            // En cas d'erreur, on libère ce qui a été alloué jusque-là
-            while (i-- > 0)
-                free(env_copy[i]);
-            free(env_copy);
-            return NULL;
-        }
-    }
-
-    env_copy[count] = NULL; // Terminer par NULL
-    return env_copy;
+	count = 0;
+	i = 0;
+	while (envp[i])
+		i++;
+	env_copy = malloc(sizeof(char *) * (i + 1));
+	if (!env_copy)
+		return (NULL);
+	while (count < i)
+	{
+		env_copy[count] = strdup(envp[count]);
+		if (!env_copy[i])
+		{
+			while (i-- > 0)
+				free(env_copy[i]);
+			free(env_copy);
+			return (NULL);
+		}
+		i++;
+	}
+	env_copy[i] = NULL;
+	return (env_copy);
 }
