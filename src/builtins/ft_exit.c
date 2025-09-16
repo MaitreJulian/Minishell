fb77edb0b4f814328a1e6887025dfefb9ba4559a
MaitreJulian <julian.venka@outlook.be>
2025-09-16 11:40:30 +0200

    exit corr

--- src/builtins/ft_exit.c
+++ src/builtins/ft_exit.c
@@ -6,11 +6,18 @@
 /*   By: jvenkata <jvenkata@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 14:15:50 by jvenkata          #+#    #+#             */
-/*   Updated: 2025/09/16 11:29:14 by jvenkata         ###   ########.fr       */
+/*   Updated: 2025/09/16 11:39:30 by jvenkata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void exit2(t_data *data, int exit_code)
+{
+	free_everything(data);
+	printf("exit\n");
+	exit (exit_code);
+}
 
 void	ft_exit(t_data *data, char *code)
 {
@@ -20,15 +27,16 @@ void	ft_exit(t_data *data, char *code)
 	i = 0;
 	if (!code)
 	{
-		printf("exit\n");
-		free_everything (data);
-		exit(0);
+		exit2(data, 0);
 	}
 	exit_code = ft_atoi(code);
 	while (code[i] && ft_isdigit(code[i]))
 		i++;
 	if (code[i] && !ft_isdigit(code[i]))
+	{
 		printf("exit : %s: numeric argument required\n", code);
+		return ;
+	}
 	if (exit_code > 255 && exit_code < 9223372036854775807)
 		exit_code = exit_code % 256;
 	else if (exit_code > 9223372036854775807)
@@ -36,7 +44,5 @@ void	ft_exit(t_data *data, char *code)
 		printf("exit : %s: numeric argument required\n", code);
 		return ;
 	}
-	free_everything(data);
-	printf("exit\n");
-	exit (exit_code);
+	exit2(data, exit_code);
 }
